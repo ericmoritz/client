ab4576c15b804f7eacea43a355f58e4ad99fca67
Eric Moritz <eric@themoritzfamily.com>
2013-08-19 15:49:47 -0400

    used compose_all instead of the + hack

--- theclient.py
+++ theclient.py
@@ -1,20 +1,23 @@
 from client.http.requests_client import get, content
 from client.content_types import json
 from client.template import template
-from client.spec import specdoc, name
+from client.spec import autodoc
 from client.debug import trace
+from client.compose import compose_all
 from pprint import pprint
 
 
-get_page = name("get_page")(
-    json + 
-    content + 
-    get() + 
-    trace("url:") + 
-    template("http://httpbin.org/get{?slug,page}", required=("slug", ))
+get_page = autodoc(
+    compose_all(
+        json, content, get(), trace("url:"),
+        template("http://httpbin.org/get{?slug,page}", required=("slug", ))
+   ),
+    name="get_page",
+    doc="Returns a simulated restful call"
 )
 
 
-print specdoc(get_page)
+print get_page.__doc__
+print
 pprint(get_page(slug="ericmoritz"))
 pprint(get_page(slug="ericmoritz", page=1))
